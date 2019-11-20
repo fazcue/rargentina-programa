4694b16a72e597d06f9fe1881269a5fe894cb0bf
Facundo Azcue <53307135+fazcue@users.noreply.github.com>
2019-11-20 18:25:18 -0300

    Reorganiza archivos de clase 4
    Eliminado arrays.js de carpeta principal (se crea posteriormente en subcarpeta "clase4").
    
    Se añade además, ejercicio fizzbuzz.js

--- clase4/arrays.js
+++ clase4/arrays.js
@@ -64,21 +64,3 @@ function calcularPromedio(notas) {
 }
 
 console.log('(Funcion) El promedio es ' + calcularPromedio(notas));
-
-//Tarea FizzBuzz del 1 al 50 (Fizz en multiplos de 3, Buzz en multiplos de 5, FizzBuzz en multiplos de ambos, resto imprimir el numero)
-//utilizando modulo (%), el cual calculo el resto
-
-function fizzBuzz() {
-    for (i = 1; i <= 50; i++) {
-        let texto = '';
-        if (i % 3 === 0) { //Si es multiplo de 3, imprimir Fizz
-            texto = 'Fizz';
-        }
-        if (i % 5 === 0) { //Sies multiplo de 5, imprimir Buzz
-            texto += 'Buzz';
-        }
-        console.log(texto || i);
-    }
-}
-
-fizzBuzz();
