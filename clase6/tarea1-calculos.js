7895b9536a16a184f022e59026b9dc814778db4b
Facundo Azcue <53307135+fazcue@users.noreply.github.com>
2019-11-27 18:54:21 -0300

    Tarea 2, clase 6
    1era parte completada

---
+++ clase6/tarea1-calculos.js
@@ -0,0 +1,48 @@
+function mayorSalarioAnual(salarios) {
+    let mayorSalarioAnual = 0;
+
+    for (i=0; salarios.length > i; i++) {
+        if (salarios[i] > mayorSalarioAnual) {
+            mayorSalarioAnual = salarios[i];
+        }
+    }
+
+    return mayorSalarioAnual;
+}
+
+function menorSalarioAnual(salarios) {
+    let menorSalarioAnual;
+
+    for (i=0; salarios.length > i; i++) {
+        if (menorSalarioAnual === undefined) {
+            menorSalarioAnual = salarios[i];
+        } else if (salarios[i] < menorSalarioAnual) {
+            menorSalarioAnual = salarios[i];
+        }
+    }
+
+    return menorSalarioAnual;
+}
+
+function promedioSalarioAnual(salarios) {
+    let total = 0;
+    let promedioSalarioAnual = 0;
+    let cantidadSalarios = 0;
+
+    for (i=0; salarios.length > i; i++) {
+        total += salarios[i];
+        cantidadSalarios++;
+    }
+
+    promedioSalarioAnual = total / cantidadSalarios;
+
+    return promedioSalarioAnual;
+}
+
+function promedioSalarioMensual(salarios) {
+    let promedioSalarioAnual = promedioSalarioAnual(salarios);
+
+    let promedioSalarioMensual = promedioSalarioAnual / 12;
+
+    return promedioSalarioMensual
+}
